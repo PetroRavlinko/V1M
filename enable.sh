9b0481fb69b2ebfe53ce143ed086b411fde4c7c7
Yaroslav Ravlinko <yaroslav.ravlinko@gmail.com>
2016-06-18 16:30:22 +0300

    Fix enable.sh script
    Modify backup procedure to create .vim folder after it will be
    created
    
    IssueID: none

--- enable.sh
+++ enable.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-# Setup vim  
+# Backup vim
+if [[ -d '~/.vim' ]]; then
+  mv -f ~/.vim ~/.vim.previous
+fi
 if [[ -f '~/.vimrc' ]]; then
   mv ~/.vimrc ~/.vimrc.previous
 fi
+
+# Setup vim  
+mkdir -v ~/.vim
 cp -v vimrc ~/.vim/vimrc
 cp -v vimrc.neovim ~/.vim/vimrc.neovim
 
 # Install vim modules
-if [[ -d '~/.vim' ]]; then
-  mv -f ~/.vim ~/.vim.previous
-fi
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 # Install Plugins
 vim +NeoBundleInstall
